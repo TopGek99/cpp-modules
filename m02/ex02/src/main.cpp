90804e7bd7211767c9ba9f575ba958009adfd1e1
TopGek <arowe890@gmail.com>
2022-11-29 17:00:25 +1030

    m02ex02: added preliminary test cases for comparison operators

--- m02/ex02/src/main.cpp
+++ m02/ex02/src/main.cpp
@@ -1,8 +1,19 @@
 #include "BestFixed.hpp"
 
 int main(void) {
-	BestFixed const b( 16777215 );
-	BestFixed const c( 42.42f );
-	std::cout << b + c << std::endl;
+	BestFixed const b( 167 );
+	BestFixed const c( 167.2f );
+	if (b > c)
+		std::cout << "b wins" << std::endl;
+	if (b < c)
+		std::cout << "c wins" << std::endl;
+	if (b >= c)
+		std::cout << "b wins or tie" << std::endl;
+	if (b <= c)
+		std::cout << "c wins or tie" << std::endl;
+	if (b == c)
+		std::cout << "tie" << std::endl;
+	if (b != c)
+		std::cout << "not tie" << std::endl;
 	return 0;
 }
