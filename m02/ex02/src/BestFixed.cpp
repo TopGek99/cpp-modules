c0bbe36ee1dfeef7d1211a3bd116bec4ab406a67
TopGek <arowe890@gmail.com>
2022-11-29 17:00:58 +1030

    m02ex02: comment out print statements for member functions

--- m02/ex02/src/BestFixed.cpp
+++ m02/ex02/src/BestFixed.cpp
@@ -1,76 +1,76 @@
 #include "BestFixed.hpp"
 
 BestFixed::BestFixed() {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 	value = 0;
 }
 
 BestFixed::BestFixed(const BestFixed &old) {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	*this = old;
 }
 
 BestFixed::BestFixed(int const val) {
-	std::cout << "Int constructor called" << std::endl;
+	// std::cout << "Int constructor called" << std::endl;
 	value = val << bits;
 }
 
 
 BestFixed::BestFixed(float const val) {
-	std::cout << "Float constructor called" << std::endl;
+	// std::cout << "Float constructor called" << std::endl;
 	value = roundf(val * (1 << bits));
 }
 
 BestFixed &BestFixed::operator=(const BestFixed &num) {
-	std::cout << "Copy assignment operator called" << std::endl;
+	// std::cout << "Copy assignment operator called" << std::endl;
 	value = num.getRawBits();
 	return (*this);
 }
 
-bool BestFixed::operator>(const BestFixed &num) {
-	std::cout << "Greater-than comparison operator called" << std::endl;
+bool BestFixed::operator>(const BestFixed &num) const {
+	// std::cout << "Greater-than comparison operator called" << std::endl;
 	return (value > num.getRawBits());
 }
 
-bool BestFixed::operator<(const BestFixed &num) {
-	std::cout << "Less-than comparison operator called" << std::endl;
+bool BestFixed::operator<(const BestFixed &num) const {
+	// std::cout << "Less-than comparison operator called" << std::endl;
 	return (value < num.getRawBits());
 }
 
-bool BestFixed::operator>=(const BestFixed &num) {
-	std::cout << "Greater-than-equal-to comparison operator called" << std::endl;
+bool BestFixed::operator>=(const BestFixed &num) const {
+	// std::cout << "Greater-than-equal-to comparison operator called" << std::endl;
 	return (value >= num.getRawBits());
 }
 
-bool BestFixed::operator<=(const BestFixed &num) {
-	std::cout << "Less-than-equal-to comparison operator called" << std::endl;
+bool BestFixed::operator<=(const BestFixed &num) const {
+	// std::cout << "Less-than-equal-to comparison operator called" << std::endl;
 	return (value <= num.getRawBits());
 }
 
-bool BestFixed::operator==(const BestFixed &num) {
-	std::cout << "Equal-to comparison operator called" << std::endl;
+bool BestFixed::operator==(const BestFixed &num) const {
+	// std::cout << "Equal-to comparison operator called" << std::endl;
 	return (value == num.getRawBits());
 }
 
-bool BestFixed::operator!=(const BestFixed &num) {
-	std::cout << "Equal-to comparison operator called" << std::endl;
+bool BestFixed::operator!=(const BestFixed &num) const {
+	// std::cout << "Equal-to comparison operator called" << std::endl;
 	return (value != num.getRawBits());
 }
 
 BestFixed BestFixed::operator+(const BestFixed &num) const {
-	std::cout << "Plus arithmetic operator called" << std::endl;
+	// std::cout << "Plus arithmetic operator called" << std::endl;
 	BestFixed res;
 	res.setRawBits(value + num.getRawBits());
 	return (res);
 }
 
 int BestFixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
+	// std::cout << "getRawBits member function called" << std::endl;
 	return (value);
 }
 
 void BestFixed::setRawBits(int const raw) {
-	std::cout << "setRawBits member function called" << std::endl;
+	// std::cout << "setRawBits member function called" << std::endl;
 	value = raw;
 }
 
@@ -83,7 +83,7 @@ float BestFixed::toFloat(void) const {
 }
 
 BestFixed::~BestFixed() {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
 
 std::ostream& operator<< (std::ostream& stream, const BestFixed& Fixed) {
