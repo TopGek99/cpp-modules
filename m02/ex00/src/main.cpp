5d67b41ef35773ce06aa8c27bf39313eef48d2a2
TopGek <arowe890@gmail.com>
2022-11-29 16:19:43 +1030

    m02ex00: improved tests

--- m02/ex00/src/main.cpp
+++ m02/ex00/src/main.cpp
@@ -1,12 +1,19 @@
 #include "Fixed.hpp"
 
 int main(void) {
+	//Default constructor
 	Fixed a;
+	//Copy constructor
 	Fixed b( a );
+	//Default constructor
 	Fixed c;
+	//setRawBits
+	b.setRawBits(8);
+	//Copy assignment operator overload
 	c = b;
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	//getRawBits
+	std::cout << "raw bits of a:\n" << a.getRawBits() << std::endl;
+	std::cout << "raw bits of b:\n" << b.getRawBits() << std::endl;
+	std::cout << "raw bits of c:\n" << c.getRawBits() << std::endl;
 	return 0;
 }
