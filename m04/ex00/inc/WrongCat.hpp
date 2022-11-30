b99e95063638191aabe09d3ff990ecabf199ab09
TopGek99 <arowe890@gmail.com>
2022-11-30 14:01:33 +1030

    m04ex00: created WrongCat class for demonstration purposes

---
+++ m04/ex00/inc/WrongCat.hpp
@@ -0,0 +1,17 @@
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+
+#include "WrongAnimal.hpp"
+#include <string>
+#include <iostream>
+
+class WrongCat : public WrongAnimal {
+	public:
+		WrongCat();
+		WrongCat(WrongCat &old);
+		WrongCat &operator=(WrongCat &old);
+		void makeSound(void);
+		~WrongCat();
+};
+
+#endif
