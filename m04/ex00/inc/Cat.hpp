d5b7a7c926b7703ec5b257b8b457db502dd542df
TopGek99 <arowe890@gmail.com>
2022-11-30 11:56:28 +1030

    m04ex00: header file for Cat class

---
+++ m04/ex00/inc/Cat.hpp
@@ -0,0 +1,16 @@
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+#include <string>
+#include <iostream>
+
+class Cat : public Animal {
+	public:
+		Cat();
+		Cat(Cat &old);
+		void makeSound(void);
+		~Cat();
+};
+
+#endif
