7e5df452492181f331d3ac06fc32327da6cd7216
TopGek99 <arowe890@gmail.com>
2022-11-30 14:02:47 +1030

    m04ex00: added code to demonstrate issue with Wrong classes

--- m04/ex00/src/main.cpp
+++ m04/ex00/src/main.cpp
@@ -1,20 +1,29 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main(void) {
-	Animal a;
-	Cat c;
-	Dog d;
+	Animal *a = new Animal();
+	Animal *c = new Cat();
+	Animal *d = new Dog();
 
-	std::cout << "a type: " << a.getType() << std::endl;
-	std::cout << "c type: " << c.getType() << std::endl;
-	std::cout << "d type: " << d.getType() << std::endl;
+	std::cout << "a type: " << a->getType() << std::endl;
+	std::cout << "c type: " << c->getType() << std::endl;
+	std::cout << "d type: " << d->getType() << std::endl;
 
 	std::cout << "a sound: ";
-	a.makeSound();
+	a->makeSound();
 	std::cout << "c sound: ";
-	c.makeSound();
+	c->makeSound();
 	std::cout << "d sound: ";
-	d.makeSound();
+	d->makeSound();
+
+	WrongAnimal *wa = new WrongAnimal();
+	WrongAnimal *wc = new WrongCat();
+
+	std::cout << "wrong a sound: ";
+	wa->makeSound();
+	std::cout << "wrong c sound: ";
+	wc->makeSound();
 	return (0);
 }
