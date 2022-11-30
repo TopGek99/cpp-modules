1f2e7fbaac513ccc385676473c3bdc124a4ca199
TopGek99 <arowe890@gmail.com>
2022-11-30 12:27:24 +1030

    m04ex01: added allocation and deletion of brain to Dog

--- m04/ex01/src/Dog.cpp
+++ m04/ex01/src/Dog.cpp
@@ -3,16 +3,19 @@
 Dog::Dog() {
 	std::cout << "Dog default constructor called" << std::endl;
 	type = "Dog";
+	b = new Brain();
 }
 
 Dog::Dog(Dog &old) : Animal(old) {
 	std::cout << "Dog copy constructor called" << std::endl;
 	type = old.type;
+	b = old.b;
 }
 
 Dog &Dog::operator=(Dog &old) {
 	std::cout << "Dog assignment operator called" << std::endl;
 	type = old.type;
+	b = old.b;
 	return (*this);
 }
 
@@ -22,4 +25,5 @@ void Dog::makeSound(void) {
 
 Dog::~Dog() {
 	std::cout << "Dog destructor called" << std::endl;
+	delete b;
 }
