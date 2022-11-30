36e5e84543322af288b540f6aff4e0169b603125
TopGek99 <arowe890@gmail.com>
2022-11-30 11:56:40 +1030

    m04ex00: header file for Dog class

---
+++ m04/ex00/inc/Dog.hpp
@@ -0,0 +1,16 @@
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+#include <string>
+#include <iostream>
+
+class Dog : public Animal {
+	public:
+		Dog();
+		Dog(Dog &old);
+		void makeSound(void);
+		~Dog();
+};
+
+#endif
