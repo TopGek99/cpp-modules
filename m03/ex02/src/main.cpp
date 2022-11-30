2451ea1b871c88ea96978c55bbc982855d2dd1d1
TopGek99 <arowe890@gmail.com>
2022-11-30 11:27:49 +1030

    m03ex02: altered tests to fit FragTrap stats

--- m03/ex02/src/main.cpp
+++ m03/ex02/src/main.cpp
@@ -1,17 +1,17 @@
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main(void) {
-	ScavTrap a;
-	ScavTrap b("john");
-	ScavTrap c(a);
-	ScavTrap d;
+	FragTrap a;
+	FragTrap b("john");
+	FragTrap c(a);
+	FragTrap d;
 
 	d = b;
 	// attack member function
 	a.attack("john");
-	// take 20 damage from attack and repair damage
-	b.takeDamage(20);
-	b.beRepaired(20);
+	// take 30 damage from attack and repair damage
+	b.takeDamage(30);
+	b.beRepaired(30);
 	// try repair but cannot as already full
 	b.beRepaired(1);
 
@@ -20,11 +20,11 @@ int main(void) {
 	c.attack("john");
 	c.beRepaired(10);
 
-	// attacks 50 times, expending all energy points and then attack and beRepaired no longer do anything
-	for (int i=0;i<50;i++) d.attack("someone");
+	// attacks 100 times, expending all energy points and then attack and beRepaired no longer do anything
+	for (int i=0;i<100;i++) d.attack("someone");
 	d.attack("guy");
 	d.beRepaired(0);
 
-	// guardGate member function
-	d.guardGate();
+	// highFivesGuys member function
+	d.highFivesGuys();
 }
