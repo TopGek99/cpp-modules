4491aebf77a4514d7a3ffff03bf9b8d48847adac
TopGek99 <arowe890@gmail.com>
2022-11-30 11:38:02 +1030

    m04ex00: added files from last exercise in last module as a start

---
+++ m04/ex00/inc/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class ClapTrap {
+	public:
+		ClapTrap();
+		ClapTrap(std::string n);
+		ClapTrap(ClapTrap &old);
+		ClapTrap &operator=(ClapTrap &old);
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		~ClapTrap();
+	protected:
+		std::string name;
+		int hp;
+		int ep;
+		int dmg;
+		int maxhp;
+};
+
+#endif
