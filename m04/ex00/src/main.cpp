1a106ca98d4d6193f0161003880ff306e2d87de3
TopGek99 <arowe890@gmail.com>
2022-11-30 11:57:57 +1030

    m04ex00: preliminary tests

--- m04/ex00/src/main.cpp
+++ m04/ex00/src/main.cpp
@@ -1,30 +1,20 @@
-#include "FragTrap.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
 
 int main(void) {
-	FragTrap a;
-	FragTrap b("john");
-	FragTrap c(a);
-	FragTrap d;
+	Animal a;
+	Cat c;
+	Dog d;
 
-	d = b;
-	// attack member function
-	a.attack("john");
-	// take 30 damage from attack and repair damage
-	b.takeDamage(30);
-	b.beRepaired(30);
-	// try repair but cannot as already full
-	b.beRepaired(1);
+	std::cout << "a type: " << a.getType() << std::endl;
+	std::cout << "c type: " << c.getType() << std::endl;
+	std::cout << "d type: " << d.getType() << std::endl;
 
-	// takes 100 damage to reach 0 HP, attack and beRepaired methods no longer do anything
-	c.takeDamage(100);
-	c.attack("john");
-	c.beRepaired(10);
-
-	// attacks 100 times, expending all energy points and then attack and beRepaired no longer do anything
-	for (int i=0;i<100;i++) d.attack("someone");
-	d.attack("guy");
-	d.beRepaired(0);
-
-	// highFivesGuys member function
-	d.highFivesGuys();
+	std::cout << "a sound: ";
+	a.makeSound();
+	std::cout << "c sound: ";
+	c.makeSound();
+	std::cout << "d sound: ";
+	d.makeSound();
+	return (0);
 }
