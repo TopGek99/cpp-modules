67f3f1f0d8130727fde3b8606138dfa81e7262a2
TopGek99 <arowe890@gmail.com>
2022-11-30 16:48:27 +1030

    m04ex01: changes to tests

--- m04/ex01/src/main.cpp
+++ m04/ex01/src/main.cpp
@@ -2,16 +2,21 @@
 #include "Cat.hpp"
 
 int main(void) {
-	Animal *animals[10];
-	Dog a;
-	Dog b(a);
+	{
+		Animal *animals[10];
+		Dog a;
+		Dog b(a);
 
-	for (int i=0;i<10;i++) {
-		if (i < 5)
-			animals[i] = new Dog();
-		else
-			animals[i] = new Cat();
+		for (int i=0;i<10;i++) {
+			if (i < 5)
+				animals[i] = new Dog();
+			else
+				animals[i] = new Cat();
+		}
+		for (int i=0;i<10;i++)
+			delete animals[i];
 	}
-	for (int i=0;i<10;i++)
-		delete animals[i];
+	{
+		
+	}
 }
