b33e746c2407f875446d65d9e4be7a008d743394
TopGek <arowe890@gmail.com>
2022-12-13 19:42:38 +1030

    m04ex01: removed extraneous brackets from main.cpp

--- m04/ex01/src/main.cpp
+++ m04/ex01/src/main.cpp
@@ -2,18 +2,16 @@
 #include "Cat.hpp"
 
 int main(void) {
-	{
-		Animal *animals[10];
-		Dog a;
-		Cat b;
+	Animal *animals[10];
+	Dog a;
+	Cat b;
 
-		for (int i=0;i<10;i++) {
-			if (i < 5)
-				animals[i] = new Dog(a);
-			else
-				animals[i] = new Cat(b);
-		}
-		for (int i=0;i<10;i++)
-			delete animals[i];
+	for (int i=0;i<10;i++) {
+		if (i < 5)
+			animals[i] = new Dog(a);
+		else
+			animals[i] = new Cat(b);
 	}
+	for (int i=0;i<10;i++)
+		delete animals[i];
 }
